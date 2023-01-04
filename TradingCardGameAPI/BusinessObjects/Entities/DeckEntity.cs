6146e1ee80cd4714cd3561445e9153f78bd24637
RaoulWo <if21b142@technikum-wien.at>
2023-01-04 17:05:04 +0100

    Implement deck entity, facade and repository

---
+++ TradingCardGameAPI/BusinessObjects/Entities/DeckEntity.cs
@@ -0,0 +1,14 @@
+﻿using BusinessObjects.Base;
+using BusinessObjects.Interfaces;
+
+namespace BusinessObjects.Entities;
+
+public class DeckEntity : Entity, IAggregateRoot
+{
+    public Guid FkCollectionId { get; set; }
+
+    protected override void Validate()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ TradingCardGameAPI/BusinessObjects/Interfaces/Facades/IDeckFacade.cs
@@ -0,0 +1,12 @@
+﻿using BusinessObjects.Entities;
+
+namespace BusinessObjects.Interfaces.Facades;
+
+public interface IDeckFacade
+{
+    IEnumerable<DeckEntity> GetAll();
+    DeckEntity GetByGuid(Guid guid);
+    int Insert(DeckEntity deck);
+    int Update(DeckEntity deck);
+    int DeleteByGuid(Guid guid);
+}
